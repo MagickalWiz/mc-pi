7e66118f7bf7f0512d9cb97b3b4c439b7a00228a
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2020-02-10 13:29:52 -0800

    Add files via upload

--- Invis.py
+++ Invis.py
@@ -1,16 +1,16 @@
 from mcpi.minecraft import Minecraft
 from mcpi import block
 import random
-from time import sleep
 
 def init():
-     #mc = Minecraft.create("192.168.7.226", 4711)
+     #mc = Minecraft.create("192.168.7.203", 4711)
      mc = Minecraft.create("127.0.0.1", 4711)
      mc.setting("world_immutable",False)
      return mc
 
 def While(mc,x,y,z,):
 	done = 0
+	mc.postToChat("Invis w/ Truesight")
 	while(done < 500):
 		h = random.randint(0,20)
 		l = random.randint(0,20)
